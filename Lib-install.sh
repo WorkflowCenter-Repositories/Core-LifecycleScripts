d8e9cd1c53907cab4992ef1dbfafef232ef89fd5
DTDWD <r.qasha@newcastle.ac.uk>
2016-05-30 15:19:38 +0100

    updating Lib-install script

--- Lib-install.sh
+++ Lib-install.sh
@@ -7,7 +7,7 @@ Lib_URL=$2
 Lib_name=$(ctx node properties lib_name)
 Lib_path=$(ctx node properties lib_path)
 
-sudo docker exec -it ${CONTAINER_NAME} test -f $Lib_path/$Lib_name && exit 0
+sudo docker exec -it ${CONTAINER_NAME} test -f $Lib_path/$Lib_name || test -f $Lib_name && exit 0
         set +e
 	  Wget=$(sudo docker exec -it ${CONTAINER_NAME} which wget)
         set -e
@@ -18,7 +18,13 @@ sudo docker exec -it ${CONTAINER_NAME} test -f $Lib_path/$Lib_name && exit 0
 
 sudo docker exec -it ${CONTAINER_NAME} test -d $Lib_path && sudo docker exec -it ${CONTAINER_NAME} rm -rf $Lib_path
 
-sudo docker exec -it ${CONTAINER_NAME} test ! -f $Lib_path.tar.gz && sudo docker exec -it ${CONTAINER_NAME} wget ${Lib_URL}
+file_name=$(basename "$Lib_URL")
+sudo docker exec -it ${CONTAINER_NAME} test ! -f $file_name && sudo docker exec -it ${CONTAINER_NAME} wget ${Lib_URL}
 
-sudo docker exec -it ${CONTAINER_NAME} gzip -t $Lib_path.tar.gz 2>/dev/null
-[[ $? -eq 0 ]] && sudo docker exec -it ${CONTAINER_NAME} tar -zxvf $Lib_path.tar.gz
+tar="tar.gz"
+set +e
+sudo docker exec -it ${CONTAINER_NAME} test "${file_name#*$tar}" != "$file_name" && sudo docker exec -it ${CONTAINER_NAME} tar -zxvf $file_name
+
+sudo docker exec -it ${CONTAINER_NAME} chmod -R 777 $Lib_path/$Lib_name
+set -e
+
