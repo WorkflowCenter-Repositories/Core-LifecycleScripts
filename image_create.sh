15cd05d66e9ef3ba23fe319271ec212f01708591
Rawaa Qasha <r.qasha@newcastle.ac.uk>
2016-06-08 12:25:57 +0100

    Update image_create.sh

--- image_create.sh
+++ image_create.sh
@@ -4,19 +4,21 @@ set -e
 image=$1
 CONTAINER_ID=$2
 create_image=$3
-
+if [[ $create_image = "True" ]]; then
 ###### get base image of task container ######
    container=$(sudo docker ps -a | grep ${CONTAINER_ID})
    b=$(echo $container | cut -d ' ' -f2)
    base=$(echo "${b//:}")
-
+   
+set +e   
 tag=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
 branch=$(git rev-parse --abbrev-ref HEAD)
 wf=$(basename `git rev-parse --show-toplevel`)
 
 image=dtdwd/$base-$wf-$branch:$tag
 image=${image,,}
-
-if [[ "$(docker images -q $image 2> /dev/null)" = "" && $create_image = "True" ]]; then
+set -e
+if [[ "$(docker images -q $image 2> /dev/null)" = "" ]]; then
  sudo docker commit -m "new ${image} image" -a "rawa" ${CONTAINER_ID} $image
+fi
 fi
